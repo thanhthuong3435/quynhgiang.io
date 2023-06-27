fceb1054e7901da00da405947285cfd910179d7e
thanhthuong3435 <85285327+thanhthuong3435@users.noreply.github.com>
2023-06-27 12:56:21 +0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,15 @@
+const CONFIG = {
+    introTitle: 'Hello công chúa!',
+    introDesc: `Công chúa trả lời câu hỏi này nhé <3`,
+    btnIntro: 'Oke',
+    title: 'Công chúa đang nhớ tui ',
+    desc: 'Đúng không?',
+    btnYes: 'Đúng rồi',
+    btnNo: 'Không:3',
+    question:'Hihi biết mà ?',
+    btnReply: 'Công chúa sẽ gửi ảnh cho tui nhé',
+    mess: ' Đấy công chúa tự nhập đấy nhá, nhớ gửi',
+    messDesc: 'Tạm biệt ',
+    btnAccept: 'Okiiii',
+    messLink: 'https://www.instagram.com/th_thg16.04' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
